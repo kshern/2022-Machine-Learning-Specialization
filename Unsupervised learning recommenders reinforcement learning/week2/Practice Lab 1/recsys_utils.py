0d7b99878ce2d2dbfbaa6e78deafc1cd112988ef
Shern <kshern@live.com>
2023-06-24 12:00:13 +0800

    m

--- Unsupervised learning recommenders reinforcement learning/week2/Practice Lab 1/recsys_utils.py
+++ Unsupervised learning recommenders reinforcement learning/week2/Practice Lab 1/recsys_utils.py
@@ -44,6 +44,52 @@ def load_Movie_List_pd():
     return(mlist, df)
 
 
+# UNQ_C3
+# GRADED FUNCTION: compute_gradient
+def compute_gradient(X, y, w, b, *argv): 
+    """
+    Computes the gradient for logistic regression 
+ 
+    Args:
+      X : (ndarray Shape (m,n)) data, m examples by n features
+      y : (ndarray Shape (m,))  target value 
+      w : (ndarray Shape (n,))  values of parameters of the model      
+      b : (scalar)              value of bias parameter of the model
+      *argv : unused, for compatibility with regularized version below
+    Returns
+      dj_dw : (ndarray Shape (n,)) The gradient of the cost w.r.t. the parameters w. 
+      dj_db : (scalar)             The gradient of the cost w.r.t. the parameter b. 
+    """
+    m, n = X.shape
+    dj_dw = np.zeros(w.shape)
+    dj_db = 0.
+
+    ### START CODE HERE ### 
+    for i in range(m):
+        f_wb_i=sigmoid(np.dot(X[i],w)+b)
+        dj_dw += (f_wb_i-y[i])*X[i]
+        dj_db += (f_wb_i-y[i])
+    dj_dw = dj_dw/m
+    dj_db = dj_db/m
+#     for i in range(m):
+#         z_wb = None
+#         for j in range(n): 
+#             z_wb += None
+#         z_wb += None
+#         f_wb = sigmoid(np.dot(X[i],w)+b)
+        
+#         dj_db_i = None
+#         dj_db += None
+        
+#         for j in range(n):
+#             dj_dw[j] = None
+            
+#     dj_dw = None
+#     dj_db = None
+    ### END CODE HERE ###
+
+        
+    return dj_db, dj_dw
 
 
 
